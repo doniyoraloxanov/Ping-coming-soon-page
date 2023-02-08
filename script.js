7e4bca33666f57574a7ea749762ddcc144eed1e9
doniyoraloxanov <doniyoraloxanov@gmail.com>
2023-02-08 20:26:27 +0500

    Ping coming soon page ready

---
+++ script.js
@@ -0,0 +1,41 @@
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const btn = document.querySelector(".btn");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  const emailValue = email.value.trim();
+
+  if (emailValue === "") {
+    setErrorFor(email, "Email cannot be blank");
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "Please provide a valid email address");
+  } else {
+    setSuccessfor(email);
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement; // .form-control
+  const small = formControl.querySelector("small");
+
+  small.innerText = message;
+
+  formControl.className = "form-control error";
+}
+
+function setSuccessfor(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
